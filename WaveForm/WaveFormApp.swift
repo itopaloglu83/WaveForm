166d9f16c326de0c2e6dcc4a6475690b43e8fa3d
Ihsan TOPALOGLU <itopaloglu83@gmail.com>
2021-07-25 15:19:00 -0400

    Initial Commit

---
+++ WaveForm/WaveFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveFormApp.swift
+//  WaveForm
+//
+//  Created by İhsan TOPALOĞLU on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WaveFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
